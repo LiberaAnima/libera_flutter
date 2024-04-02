7cb0bc0ec607316b1041460ab7e18c5e0f1be4d1
hwacho9 <csh77776@gmail.com>
2024-04-02 22:21:09 +0900

    feat:emailinput, passwordinput, loginbutton

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,6 +10,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _key = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -20,41 +22,86 @@ class _LoginPageState extends State<LoginPage> {
         title: const Text("Login"),
       ),
       body: Center(
+        key: _key,
         child: Column(
           children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: "Email",
-              ),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: "Password",
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  await _auth.signInWithEmailAndPassword(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                  );
-                  Navigator.pushNamed(context, '/');
-                } on FirebaseAuthException catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("e"),
-                    ),
-                  );
-                }
-              },
-              child: const Text("Login"),
-            ),
+            emailInput(),
+            passwordInput(),
+            loginButton(),
           ],
         ),
       ),
+    );
+  }
+
+  TextFormField emailInput() {
+    return TextFormField(
+      controller: _emailController,
+      autofocus: true,
+      validator: (val) {
+        if (val!.isEmpty) {
+          return "Please enter your email";
+        } else {
+          return null;
+        }
+      },
+      decoration: const InputDecoration(
+        hintText: "Input your Email Address",
+        labelText: "Email Address",
+        // labelStyle: TextStyle(
+        //   fontSize: 18,
+        //   fontWeight: FontWeight.bold,
+        // ),
+      ),
+    );
+  }
+
+  TextFormField passwordInput() {
+    return TextFormField(
+      controller: _passwordController,
+      autofocus: true,
+      validator: (val) {
+        if (val!.isEmpty) {
+          return "Please enter your password";
+        } else {
+          return null;
+        }
+      },
+      decoration: const InputDecoration(
+        hintText: "Input your Password",
+        labelText: "Password",
+        // labelStyle: TextStyle(
+        //   fontSize: 18,
+        //   fontWeight: FontWeight.bold,
+        // ),
+      ),
+    );
+  }
+
+  ElevatedButton loginButton() {
+    return ElevatedButton(
+      onPressed: () async {
+        try {
+          await _auth
+              .signInWithEmailAndPassword(
+                email: _emailController.text,
+                password: _passwordController.text,
+              )
+              .then((_) => Navigator.pushNamed(context, '/'));
+        } on FirebaseAuthException catch (e) {
+          if (e.code == 'user-not-found') {
+            debugPrint('No user found for that email');
+          } else if (e.code == 'wrong-password') {
+            debugPrint('Wrong password provided for that user');
+          }
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("test"),
+            ),
+          );
+        }
+      },
+      child: const Text("Login"),
     );
   }
 }
