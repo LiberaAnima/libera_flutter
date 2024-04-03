2d760d17efdf24fc98fd2318898e9e88591b2e88
ImagawaKosuke <kosuke8de@gmail.com>
2024-04-03 16:46:27 +0900

    Update pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      routes: {
+        '/': (context) => const HomePage(),
+        '/logIn': (context) => const LoginPage(),
+        '/signUp': (context) => const SignupPage(),
+      },
     );
   }
 }
