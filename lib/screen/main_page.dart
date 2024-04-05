2a2b8a58d3403f822c07a3b9da47dd4520e7eda9
hwacho9 <csh77776@gmail.com>
2024-04-06 01:39:41 +0900

    feat:get user info from db

--- lib/screen/main_page.dart
+++ lib/screen/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:libera_flutter/screen/login_page.dart';
@@ -11,6 +12,15 @@ class MainPage extends StatefulWidget {
 
 class _MainPagePageState extends State<MainPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<DocumentSnapshot> getUserInfo() async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      return await _firestore.collection('users').doc(user.uid).get();
+    }
+    throw Exception('No user logged in');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,30 +41,53 @@ class _MainPagePageState extends State<MainPage> {
           ),
         ),
         child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(style: TextStyle(color: Colors.white), "hello"),
-              ElevatedButton(
-                onPressed: () async => await _auth
-                    .signOut()
-                    .then((_) => Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginPage()),
-                          (Route<dynamic> route) => false,
-                        )),
-                child: Text("Log Out"),
-              ),
-              ElevatedButton(
-                onPressed: () => Navigator.pushNamed(context, '/post'),
-                child: Text("post"),
-              ),
-              ElevatedButton(
-                onPressed: () => Navigator.pushNamed(context, '/postbook'),
-                child: Text("postbook"),
-              )
-            ],
+          child: FutureBuilder<DocumentSnapshot>(
+            future: getUserInfo(),
+            builder: (BuildContext context,
+                AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                final data = snapshot.data!.data() as Map<String, dynamic>;
+                print(data);
+                return Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Email: ${data['email']}',
+                          style: TextStyle(color: Colors.black)),
+                      Text('Name: ${data['username']}'),
+                      Text('Gender: ${data['gender']}',
+                          style: TextStyle(color: Colors.black)),
+                      Text('School: ${data['school']}',
+                          style: TextStyle(color: Colors.black)),
+                      // ...
+                    ],
+                  ),
+                );
+              } else if (snapshot.connectionState == ConnectionState.none) {
+                return Text("No data");
+              }
+              return CircularProgressIndicator();
+            },
           ),
+
+          // ElevatedButton(
+          //   onPressed: () async => await _auth
+          //       .signOut()
+          //       .then((_) => Navigator.pushAndRemoveUntil(
+          //             context,
+          //             MaterialPageRoute(builder: (context) => LoginPage()),
+          //             (Route<dynamic> route) => false,
+          //           )),
+          //   child: Text("Log Out"),
+          // ),
+          // ElevatedButton(
+          //   onPressed: () => Navigator.pushNamed(context, '/post'),
+          //   child: Text("post"),
+          // ),
+          // ElevatedButton(
+          //   onPressed: () => Navigator.pushNamed(context, '/postbook'),
+          //   child: Text("postbook"),
+          // )
         ),
       ),
     );
