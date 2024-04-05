e48080dbed4ad51477d3e70e3fac9628a52bf38b
hwacho9 <csh77776@gmail.com>
2024-04-06 00:25:29 +0900

    feat:signup2page

--- lib/screen/signup_page.dart
+++ lib/screen/signup_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:libera_flutter/screen/signup2_page.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({Key? key}) : super(key: key);
@@ -93,15 +94,24 @@ class _SignupPageState extends State<SignupPage> {
             );
 
             final user = credential.user;
-            await FirebaseFirestore.instance
-                .collection('users')
-                .doc(user?.uid)
-                .set({
-              'email': _emailController.text,
-              // 다른 필드를 추가하세요.
-            });
+            print(user?.uid);
+            // await FirebaseFirestore.instance
+            //     .collection('users')
+            //     .doc(user?.uid)
+            //     .set(
+            //   {
+            //     'email': _emailController.text,
+            //     'uid': user?.uid,
+            //     // 다른 필드를 추가하세요.
+            //   },
+            // );
 
-            Navigator.pushNamed(context, "/");
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Signup2Page(userCredential: credential),
+              ),
+            );
 
             ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(
