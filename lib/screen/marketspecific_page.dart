0d98ecdede297ecb9bcd16becbe1e19e55f9c0fb
shokubohcm <shokubota7280@gmail.com>
2024-04-07 13:46:23 +0900

    make base UI of marketspecific page

--- lib/screen/marketspecific_page.dart
+++ lib/screen/marketspecific_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: MarketSpecificPage(),
+  ));
+}
+
 class MarketSpecificPage extends StatefulWidget {
   const MarketSpecificPage({Key? key}) : super(key: key);
 
@@ -12,14 +18,87 @@ class _MarketSpecificPageState extends State<MarketSpecificPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("市場別画面"),
+        title: Text("商品詳細画面"),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text("市場別画面"),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          Image.network(
+              "https://firebasestorage.googleapis.com/v0/b/libera-b72ea.appspot.com/o/books%2F1712298476555.jpg?alt=media&token=31ef49ba-4b6d-4b37-8848-5e088288aa98",
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage:
+                      NetworkImage('https://example.com/user-icon.jpg'),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('ユーザーネーム: uiduid'),
+                    Row(
+                      children: [
+                        Text('関西学院大学'),
+                        SizedBox(width: 10),
+                        Text('文学部'),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'キャリアで語る経営組織',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  '¥1,000',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('文学部'),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('三日前'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // ボタンが押された時のアクション
+        },
+        label: Text('チャットする'),
+        icon: Icon(Icons.chat),
+        backgroundColor: Colors.orange,
+        shape: StadiumBorder(),
       ),
     );
   }
