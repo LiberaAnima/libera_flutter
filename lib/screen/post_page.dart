13e0f8637ae896c54362af6d53f10b364ff84db1
hwacho9 <csh77776@gmail.com>
2024-04-07 18:38:41 +0900

    fix:post_page

--- lib/screen/post_page.dart
+++ lib/screen/post_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import '../main.dart';
-import 'package:intl/intl.dart';
 
 class PostPage extends StatefulWidget {
   @override
@@ -17,22 +13,6 @@ class _PostPagePageState extends State<PostPage> {
     /// 入力欄をクリアにする
     _textEditingController.clear();
   }
-
-  //ユーザー確認
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  Future<DocumentSnapshot> getUserInfo() async {
-    final User? user = _auth.currentUser;
-    if (user != null) {
-      return await _firestore.collection('users').doc(user.uid).get();
-    }
-    throw Exception('No user logged in');
-  }
-
-  //日付を取得
-  DateTime now = DateTime.now();
-  DateFormat outputFormat = DateFormat('yyyy年MM月dd日 HH:mm');
-  late String date = outputFormat.format(now);
 
   @override
   Widget build(BuildContext context) {
@@ -49,29 +29,19 @@ class _PostPagePageState extends State<PostPage> {
           //maxLengthEnforced: false, // 入力上限になったときに、文字入力を抑制するか
           style: TextStyle(color: Colors.black),
           obscureText: false,
-          maxLines: 5,
+          maxLines: 1,
           decoration: const InputDecoration(
             icon: Icon(Icons.speaker_notes),
             hintText: '投稿内容を記載します',
-            labelText: '授業はどうですか?',
+            labelText: '内容 * ',
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final User? user = _auth.currentUser;
-          if (user != null) {
-            final String uid = user.uid;
-            final DocumentSnapshot userDoc =
-                await _firestore.collection('users').doc(uid).get();
-            FirebaseFirestore.instance.collection('posts').doc().set({
-              'post_message': _textEditingController.text,
-              'date': date,
-              'name': userDoc['username'],
-            });
-            _onSubmitted(_textEditingController.text);
-            Navigator.pushNamed(context, '/');
-          }
+        onPressed: () {
+          print('_textEditingController.text: ${_textEditingController.text}');
+          // _onSubmitted(_textEditingController.text);
+          Navigator.pushNamed(context, '/');
         },
         child: Icon(Icons.send),
       ),
