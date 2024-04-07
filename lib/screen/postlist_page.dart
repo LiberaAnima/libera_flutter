82433460717e9cabe522464c13bfdd1b61245cf3
hwacho9 <csh77776@gmail.com>
2024-04-07 18:39:41 +0900

    delete unuser package

--- lib/screen/postlist_page.dart
+++ lib/screen/postlist_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 
 class PostListPage extends StatefulWidget {
   const PostListPage({Key? key}) : super(key: key);
