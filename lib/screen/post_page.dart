cf7149ac38ed438c647e66eb0d0e464c2aff6b70
ImagawaKosuke <kosuke8de@gmail.com>
2024-04-07 13:23:28 +0900

    投稿追加__UI作成前

--- lib/screen/post_page.dart
+++ lib/screen/post_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../main.dart';
 import 'package:intl/intl.dart';
 
@@ -17,9 +18,22 @@ class _PostPagePageState extends State<PostPage> {
     _textEditingController.clear();
   }
 
+  //ユーザー確認
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<DocumentSnapshot> getUserInfo() async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      return await _firestore.collection('users').doc(user.uid).get();
+    }
+    throw Exception('No user logged in');
+  }
+
+  //日付を取得
   DateTime now = DateTime.now();
   DateFormat outputFormat = DateFormat('yyyy年MM月dd日 HH:mm');
   late String date = outputFormat.format(now);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,20 +49,22 @@ class _PostPagePageState extends State<PostPage> {
           //maxLengthEnforced: false, // 入力上限になったときに、文字入力を抑制するか
           style: TextStyle(color: Colors.black),
           obscureText: false,
-          maxLines: 1,
+          maxLines: 5,
           decoration: const InputDecoration(
             icon: Icon(Icons.speaker_notes),
             hintText: '投稿内容を記載します',
-            labelText: '内容',
+            labelText: '授業はどうですか?',
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('posts')
-              .doc()
-              .set({'post_message': _textEditingController.text, 'date': date});
+        onPressed: () async {
+          //final String name = snapshot['username'];
+          FirebaseFirestore.instance.collection('posts').doc().set({
+            'post_message': _textEditingController.text,
+            'date': date,
+            'name': "aaaaaa",
+          });
           _onSubmitted(_textEditingController.text);
         },
         child: Icon(Icons.send),
