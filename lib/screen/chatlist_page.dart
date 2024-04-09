75deb7bfacf5be87d33fe7723f66e7d199576f3d
hwacho9 <csh77776@gmail.com>
2024-04-09 11:53:23 +0900

    merge from shima/feat_chat

---
+++ lib/screen/chatlist_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:libera_flutter/screen/chat_post.dart';
+
+class UserListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User List'),
+      ),
+      body: UsersList(),
+    );
+  }
+}
+
+class UsersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String myUserId = 'YQEvhBeccnZQFjbg6P7pIzrRiid2'; // 実際には動的に取得することを想定
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Center(child: Text('No users found.'));
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map((document) {
+                Map<String, dynamic> userData =
+                    document.data() as Map<String, dynamic>;
+                // ユーザー名が存在しない、または空文字列の場合はリストアイテムを作らない
+                if (userData['username'] == null ||
+                    userData['username']!.isEmpty) {
+                  return Container(); // 空のコンテナを返してアイテムを表示しない
+                } else {
+                  String username = userData['username'];
+                  return ListTile(
+                    title: Text(username),
+                    onTap: () {
+                      String conversationId =
+                          document.id; // 仮のconversationId生成ロジック
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => ChatRoom(
+                              conversationId: conversationId, userId: myUserId),
+                        ),
+                      );
+                    },
+                  );
+                }
+              })
+              .where((element) => element is! Container)
+              .toList(), // Container（空のリストアイテム）を除外する
+        );
+      },
+    );
+  }
+}
---
+++ lib/screen/chatroom_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ChatRoom(
+          conversationId: 'exampleConversationId', userId: 'exampleUserId'),
+    );
+  }
+}
+
+class ChatRoom extends StatelessWidget {
+  final String conversationId;
+  final String userId;
+
+  ChatRoom({required this.conversationId, required this.userId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat App'),
+      ),
+      body: MessagesList(conversationId: conversationId, userId: userId),
+    );
+  }
+}
+
+class MessagesList extends StatelessWidget {
+  final String conversationId;
+  final String userId;
+
+  MessagesList({required this.conversationId, required this.userId});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('Messages')
+          .where('conversationId', isEqualTo: conversationId)
+          .orderBy('timestamp', descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+
+        final messages = _firebaseMessagesToChatMessages(snapshot.data!.docs);
+
+        return Chat(
+          messages: messages,
+          onSendPressed: (types.PartialText message) async {
+            final docRef =
+                FirebaseFirestore.instance.collection('Messages').doc();
+            await docRef.set({
+              'conversationId': conversationId,
+              'senderId': userId,
+              'text': message.text,
+              'timestamp': FieldValue.serverTimestamp(),
+            });
+          },
+          user: types.User(id: userId),
+          onMessageLongPress: (context, message) {
+            if (message.author.id == userId) {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => Wrap(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.delete),
+                      title: Text('Delete Message'),
+                      onTap: () {
+                        Navigator.of(context).pop(); // Close the bottom sheet
+                        FirebaseFirestore.instance
+                            .collection('Messages')
+                            .doc(message.id)
+                            .delete();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
+          },
+        );
+      },
+    );
+  }
+
+  List<types.Message> _firebaseMessagesToChatMessages(
+      List<QueryDocumentSnapshot> messageDocs) {
+    return messageDocs.map<types.Message>((doc) {
+      final data = doc.data() as Map<String, dynamic>;
+      final Timestamp timestamp =
+          data['timestamp'] as Timestamp? ?? Timestamp.now();
+      final text = data['text'] ?? '';
+
+      return types.TextMessage(
+        author: types.User(id: data['senderId']),
+        createdAt: timestamp.toDate().millisecondsSinceEpoch,
+        id: doc.id,
+        text: text,
+      );
+    }).toList();
+  }
+}
