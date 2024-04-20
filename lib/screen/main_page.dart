80fe906a8acf8546a613547f6bcaa050d77f7a7f
hwacho9 <csh77776@gmail.com>
2024-04-20 12:46:22 +0900

    feat:go to loginpage if not logged in

--- lib/screen/main_page.dart
+++ lib/screen/main_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:libera_flutter/screen/login_page.dart';
 import 'package:libera_flutter/screen/profile_page.dart';
 import 'package:libera_flutter/services/launchUrl_service.dart';
 
@@ -25,129 +26,149 @@ class _MainPagePageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leadingWidth: 100,
-        leading: Image.asset('assets/images/icon.png'),
-        // title: Text("メイン画面"),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.notifications_none),
-          ),
-          IconButton(
-            onPressed: () {
-              final String? uid = _auth.currentUser?.uid;
-              print(uid);
+    return StreamBuilder<User?>(
+      stream: _auth.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final User? user = snapshot.data;
+          if (user == null) {
+            return LoginPage(); // Redirect to LoginPage if not logged in
+          } else {
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.white,
+                leadingWidth: 100,
+                leading: Image.asset('assets/images/icon.png'),
+                // title: Text("メイン画面"),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.search),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.notifications_none),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      final String? uid = _auth.currentUser?.uid;
+                      print(uid);
 
-              if (uid != null) {
-                // uid를 ProfilePage로 전달하면서 이동합니다.
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProfilePage(uid: uid),
-                  ),
-                );
-              }
-            },
-            icon: Icon(Icons.account_circle),
-          ),
-        ],
-      ),
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                    // get user data
-                    future: getUserData(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return CircularProgressIndicator();
-                      } else if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                      } else {
-                        final userData = snapshot.data!.data();
-                        return Row(
-                          children: [
-                            Padding(
-                              // 学校名
-                              padding: const EdgeInsets.only(left: 30),
-                              child: Text(
-                                userData?['school'],
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 30,
-                                    color: Colors.black),
-                              ),
-                            ),
-                          ],
+                      if (uid != null) {
+                        // uid를 ProfilePage로 전달하면서 이동합니다.
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProfilePage(uid: uid),
+                          ),
                         );
                       }
                     },
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Card.outlined(
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/class');
-                      },
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            width: 350,
-                            height: 150,
-                            child: Center(
-                              child: Text("今日の時間割"),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      menuIcon(Icons.home, "https://kwic.kwansei.ac.jp/login",
-                          "学校ホーム"),
-                      menuIcon(Icons.discount, "/discount", "学割"),
-                      menuIcon(Icons.discount, "/discount", "?"),
-                      menuIcon(Icons.discount, "/discount", "?"),
-                    ],
-                  ),
-                  Card.outlined(
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          width: 350,
-                          height: 150,
-                          child: Center(
-                            child: Text("何か入れる"),
-                          ),
-                        )
-                      ],
-                    ),
+                    icon: Icon(Icons.account_circle),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
-      ),
+              body: Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                            // get user data
+                            future: getUserData(),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const CircularProgressIndicator();
+                              } else if (snapshot.hasError) {
+                                return Text('Error: ${snapshot.error}');
+                              } else {
+                                final userData = snapshot.data!.data();
+                                return Row(
+                                  children: [
+                                    Padding(
+                                      // 学校名
+                                      padding: const EdgeInsets.only(left: 30),
+                                      child: Text(
+                                        userData?['school'] ?? "null",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 30,
+                                            color: Colors.black),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              }
+                            },
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          ElevatedButton(
+                            onPressed: () async => await _auth.signOut().then(
+                                (_) => Navigator.pushNamed(context, "/logIn")),
+                            child: const Text("Log Out"),
+                          ),
+                          Card.outlined(
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.pushNamed(context, '/class');
+                              },
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    width: 350,
+                                    height: 150,
+                                    child: Center(
+                                      child: Text("今日の時間割"),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              menuIcon(Icons.home,
+                                  "https://kwic.kwansei.ac.jp/login", "学校ホーム"),
+                              menuIcon(Icons.discount, "/discount", "学割"),
+                              menuIcon(Icons.discount, "/discount", "?"),
+                              menuIcon(Icons.discount, "/discount", "?"),
+                            ],
+                          ),
+                          Card.outlined(
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  width: 350,
+                                  height: 150,
+                                  child: Center(
+                                    child: Text("何か入れる"),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
   }
 
