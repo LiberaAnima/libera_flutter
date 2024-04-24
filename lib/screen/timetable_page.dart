8c02846d8379ea9bf63fe13cbc3c00bd8d85ea31
hwacho9 <csh77776@gmail.com>
2024-04-24 19:43:53 +0900

    feat:get timetable data

--- lib/screen/timetable_page.dart
+++ lib/screen/timetable_page.dart
@@ -48,7 +48,7 @@ class _TimeTablePageState extends State<TimeTablePage> {
               ),
               Padding(
                 padding:
-                    const EdgeInsets.only(top: 40.0, left: 30.0, right: 20.0),
+                    const EdgeInsets.only(top: 50.0, left: 30.0, right: 20.0),
                 child: GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 5,
@@ -56,115 +56,167 @@ class _TimeTablePageState extends State<TimeTablePage> {
                   ),
                   itemCount: 25,
                   itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () async {
-                        int period = index ~/ 5 + 1;
-                        String day = ''; // index % 5 -> room name
-                        switch (index % 5) {
-                          case 0:
-                            day = '月曜';
-                            break;
-                          case 1:
-                            day = '火曜';
-                            break;
-                          case 2:
-                            day = '水曜';
-                            break;
-                          case 3:
-                            day = '木曜';
-                            break;
-                          case 4:
-                            day = '金曜';
-                            break;
-                        }
-                        final result = await showDialog<Map<String, String>>(
-                          context: context,
-                          builder: (context) {
-                            String className = '';
-                            String roomName = '';
+                    return FutureBuilder<DocumentSnapshot>(
+                        future: FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(user?.uid)
+                            .get(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting)
+                            return CircularProgressIndicator();
+                          if (snapshot.hasError) {
+                            return Text('Something went wrong');
+                          } else {
+                            Map<String, dynamic> data =
+                                snapshot.data!.data() as Map<String, dynamic>;
+                            int period = index ~/ 5 + 1;
+                            String day = ''; // index % 5 -> room name
+                            switch (index % 5) {
+                              case 0:
+                                day = '月曜';
+                                break;
+                              case 1:
+                                day = '火曜';
+                                break;
+                              case 2:
+                                day = '水曜';
+                                break;
+                              case 3:
+                                day = '木曜';
+                                break;
+                              case 4:
+                                day = '金曜';
+                                break;
+                            }
 
-                            return AlertDialog(
-                              title: Text('$day,$period限',
-                                  textAlign: TextAlign.center),
-                              content: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  TextField(
-                                    decoration:
-                                        InputDecoration(hintText: 'Class name'),
-                                    onChanged: (value) {
-                                      className = value;
-                                    },
-                                  ),
-                                  TextField(
-                                    decoration:
-                                        InputDecoration(hintText: 'Room name'),
-                                    onChanged: (value) {
-                                      roomName = value;
-                                    },
-                                  ),
-                                ],
+                            if (data.containsKey('timetable') &&
+                                data['timetable'].containsKey(day) &&
+                                data['timetable'][day]
+                                    .containsKey(period.toString())) {
+                              List<dynamic> classInfo = data['timetable'][day]
+                                  [period.toString()] as List<dynamic>;
+                              // Display the data
+                              print(classInfo);
+                              return ListTile(
+                                title: Text("t"),
+                                // ... add more details ...
+                              );
+                            }
+                          }
+                          return GestureDetector(
+                            onTap: () async {
+                              int period = index ~/ 5 + 1;
+                              String day = ''; // index % 5 -> room name
+                              switch (index % 5) {
+                                case 0:
+                                  day = '月曜';
+                                  break;
+                                case 1:
+                                  day = '火曜';
+                                  break;
+                                case 2:
+                                  day = '水曜';
+                                  break;
+                                case 3:
+                                  day = '木曜';
+                                  break;
+                                case 4:
+                                  day = '金曜';
+                                  break;
+                              }
+                              final result =
+                                  await showDialog<Map<String, String>>(
+                                context: context,
+                                builder: (context) {
+                                  String className = '';
+                                  String roomName = '';
+
+                                  return AlertDialog(
+                                    title: Text('$day,$period限',
+                                        textAlign: TextAlign.center),
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        TextField(
+                                          decoration: InputDecoration(
+                                              hintText: 'Class name'),
+                                          onChanged: (value) {
+                                            className = value;
+                                          },
+                                        ),
+                                        TextField(
+                                          decoration: InputDecoration(
+                                              hintText: 'Room name'),
+                                          onChanged: (value) {
+                                            roomName = value;
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        child: Text('OK'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop({
+                                            'class': className,
+                                            'room': roomName,
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                              if (result != null) {
+                                setState(() {
+                                  classes[index] = result!;
+                                });
+
+                                // save the data in firebase
+
+                                DocumentReference userDoc = FirebaseFirestore
+                                    .instance
+                                    .collection('users')
+                                    .doc(user?.uid);
+                                // Add the class information to the Firestore collection
+                                userDoc.update({
+                                  'timetable.$day.$period':
+                                      FieldValue.arrayUnion(
+                                          [result!['class'], result['room']])
+                                });
+                              }
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.all(5.0),
+                              height: 200,
+                              width: 200,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(color: Colors.grey),
+                                borderRadius: BorderRadius.circular(10.0),
                               ),
-                              actions: [
-                                TextButton(
-                                  child: Text('OK'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop({
-                                      'class': className,
-                                      'room': roomName,
-                                    });
-                                  },
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      classes[index]?['class'] ?? '',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 15,
+                                    ),
+                                    Text(classes[index]?['room'] ?? ''),
+                                  ],
                                 ),
-                              ],
-                            );
-                          },
-                        );
-                        if (result != null) {
-                          setState(() {
-                            classes[index] = result!;
-                          });
-
-                          // save the data in firebase
-
-                          DocumentReference userDoc = FirebaseFirestore.instance
-                              .collection('users')
-                              .doc(user?.uid);
-                          // Add the class information to the Firestore collection
-                          userDoc.update({
-                            'timetable.$day.$period': FieldValue.arrayUnion(
-                                [result!['class'], result['room']])
-                          });
-                        }
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.all(5.0),
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                classes[index]?['class'] ?? '',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
                               ),
-                              SizedBox(
-                                height: 15,
-                              ),
-                              Text(classes[index]?['room'] ?? ''),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
+                            ),
+                          );
+                        });
                   },
                 ),
               ),
