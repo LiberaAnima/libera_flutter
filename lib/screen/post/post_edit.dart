0b2dc2dfb39f8cd6cd5cf71fee379b8dbfd710c0
hwacho9 <csh77776@gmail.com>
2024-04-29 22:34:02 +0900

    feat:post edit page

---
+++ lib/screen/post/post_edit.dart
@@ -0,0 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class PostEditPage extends StatefulWidget {
+  final Map<String, dynamic> data;
+
+  PostEditPage({required this.data});
+
+  @override
+  _PostEditPageState createState() => _PostEditPageState();
+}
+
+class _PostEditPageState extends State<PostEditPage> {
+  late TextEditingController _titleController;
+  late TextEditingController _descriptionController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.data['title']);
+    _descriptionController =
+        TextEditingController(text: widget.data['post_message'] ?? '');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(widget.data);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Post'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _titleController,
+                        decoration: InputDecoration(
+                          labelText: 'Title',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  controller: _descriptionController,
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                  ),
+                  maxLines: 6,
+                ),
+              ],
+            ),
+            // 여기에 추가적인 필드를 추가합니다.
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // 텍스트 필드에서 텍스트를 가져옵니다.
+          String title = _titleController.text;
+          String postMessage = _descriptionController.text;
+
+          // Firebase에 데이터를 업데이트합니다.
+          await FirebaseFirestore.instance
+              .collection('posts')
+              .doc(widget.data['documentID'])
+              .update({
+            'title': title,
+            'post_message': postMessage,
+          });
+
+          // 수정된 데이터를 이전 화면으로 전달합니다.
+          Navigator.pop(context, {
+            'title': title,
+            'post_message': postMessage,
+          });
+        },
+        child: Icon(Icons.save),
+      ),
+    );
+  }
+}
