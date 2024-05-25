73c5f5d6c44040e65bfee6990d651ec8e07af812
hwacho9 <csh77776@gmail.com>
2024-05-25 15:59:28 +0900

    feat:comment push

--- functions/index.js
+++ functions/index.js
@@ -28,3 +28,29 @@ exports.myFunction = functions.firestore
 
       return Promise.all(promises);
     });
+
+exports.sendNotificationOnComment = functions.firestore
+    .document("posts/{postId}/comments/{commentId}")
+    .onCreate(async (snapshot, context) => {
+      const postId = context.params.postId;
+      const newComment = snapshot.data();
+
+      // Get the post document
+      const postSnapshot = await admin.firestore().doc(`posts/${postId}`).get();
+      const postData = postSnapshot.data();
+
+      // Get the post author's uid
+      const uid = postData.uid;
+
+      // Prepare the notification
+      const notification = {
+        notification: {
+          title: "New comment on your post",
+          body: `${newComment.name}: ${newComment.text}`,
+          clickAction: "FLUTTER_NOTIFICATION_CLICK",
+        },
+      };
+
+      // Send the notification
+      return admin.messaging().sendToTopic(uid, notification);
+    });
