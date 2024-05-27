69b03cf52e60e86c6c9dba932e57c95bee00696c
hwacho9 <csh77776@gmail.com>
2024-05-27 16:57:10 +0900

    feat:discount page

--- lib/screen/discount_page.dart
+++ lib/screen/discount_page.dart
@@ -1,24 +1,70 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class DiscountPage extends StatefulWidget {
+class DiscountPage extends StatelessWidget {
   const DiscountPage({Key? key}) : super(key: key);
 
-  @override
-  _DiscountPageState createState() => _DiscountPageState();
-}
-
-class _DiscountPageState extends State<DiscountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("割引画面"),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text("割引画面"),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection('discounts').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                var document = snapshot.data!.docs[index];
+                var content = document['contents'];
+                var title = document['title'];
+                var url = document['URL'];
+                // Cut the content to a maximum of 50 characters
+                if (content.length > 50) {
+                  content = content.substring(0, 50) + '...';
+                }
+                return GestureDetector(
+                  onTap: () => launch(url),
+                  child: Card(
+                    color: Colors.white,
+                    shadowColor: Colors.grey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          const SizedBox(height: 20),
+                          Text(
+                            title,
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 20),
+                          Text(content, style: const TextStyle(fontSize: 15)),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
